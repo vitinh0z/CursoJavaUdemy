3fbd09a0d594fac4d07e01e7686bb2c2f5d1ca99
vitinh0z <victor10.vg23@gmail.com>
2025-08-23 20:04:37 -0300

    exercicio usando vetor e oop

---
+++ Vetores/MediaProdutos/Products.java
@@ -0,0 +1,47 @@
+package Vetores.MediaProdutos;
+
+public class Products {
+
+
+    private String nome;
+    private double preco;
+
+
+    
+    public Products(String nome, double preco) {
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+
+
+    public String getNome() {
+        return nome;
+    }
+
+
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+
+
+    public double getPreco() {
+        return preco;
+    }
+
+
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+
+    
+    
+
+
+
+    
+}
