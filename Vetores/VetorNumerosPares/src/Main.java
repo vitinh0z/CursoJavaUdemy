4eb4a02fa2e40778dfc6fdc64a053b556942f914
vitinh0z <victor10.vg23@gmail.com>
2025-08-25 16:24:29 -0300

    FIX: Arrumei os isPar que esta incorreto.

--- Vetores/VetorNumerosPares/src/Main.java
+++ Vetores/VetorNumerosPares/src/Main.java
@@ -21,6 +21,7 @@ public class Main {
         Numeros[] vetor = new Numeros[quantidade];
 
         for (int i = 0; i<vetor.length; i++){
+            
             System.out.printf("Digite o %d° Número: ", i+1);
             int numero = read.nextInt();
 
