7309a3404defe2b1a699be70af738f4f345ad529
Victor Gabriel <100782235+vitinh0z@users.noreply.github.com>
2025-08-27 15:46:10 -0300

    Atualizar o Main.java
    tirei o %d pois acho que ele tava pegando o valor de i

--- Vetores/MediaPares/Main.java
+++ Vetores/MediaPares/Main.java
@@ -6,12 +6,10 @@ public class Main {
         
     
         Scanner read = new Scanner(System.in);
-     
-
-
 
 
         System.out.print("Digite quantidade de elementos no VetorA: ");
+
         int quantidade = read.nextInt(); 
 
         Numeros[] vetorA = new Numeros[quantidade];
@@ -27,27 +25,17 @@ public class Main {
         int soma = 0;
 
         for (int i = 0; i<vetorA.length; i++){
-            if (vetorA[i].isPar()){
-                System.out.printf("Media dos Pares: %d", (soma+=vetorA[i].getNumeros()) / vetorA.length);
 
-            }    
+            if (vetorA[i].isPar()){
+
+                System.out.println("Media dos Pares: "+ (soma+=vetorA[i].getNumeros()) / vetorA.length);
+
+            }
+    
             else {
                 System.out.println("Não tem numeros pares");
             }
         }
-
-        
-       
-
-        
-
-
-
-
-
-
-
-
 
 
     }
