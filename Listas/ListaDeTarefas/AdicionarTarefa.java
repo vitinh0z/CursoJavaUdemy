328d892af4678c49e4766a6b798f116a4762829d
vitinh0z <victor10.vg23@gmail.com>
2025-09-04 22:58:19 -0300

    FIX: arrumei o list.add que estava duplicando as tarefas e arrumei o do-while da descrição

--- Listas/ListaDeTarefas/AdicionarTarefa.java
+++ Listas/ListaDeTarefas/AdicionarTarefa.java
@@ -1,20 +1,20 @@
 package Listas.ListaDeTarefas;
+
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class AdicionarTarefa {
 
-    public static void adicionarTarefa () {
+    public static void adicionarTarefa() {
 
-        
         String continuarTarefa;
 
         List<Tarefa> list = new ArrayList<>();
         Scanner read = new Scanner(System.in);
-        
+
         int id = 0;
-        do{
+        do {
 
             Tarefa tarefa = new Tarefa();
 
@@ -24,61 +24,61 @@ public class AdicionarTarefa {
                 try {
 
                     System.out.println("\nTitulo da tarefa");
-                    tarefa.setTitulo(read.nextLine()); 
+                    tarefa.setTitulo(read.nextLine());
                     id++;
                     tituloValido = true;
 
                     tarefa.setId(id);
 
                 } catch (IllegalArgumentException e) {
-                    
+
                     System.out.println(e.getMessage());
 
                 }
 
             } while (tituloValido == false);
 
-            
-            
-
-                    
             System.out.println("\nGostaria de colocar uma descrição para tarefa? s/n");
             String opcao = read.nextLine();
-                    
 
-            if (opcao.equals("s")){
+            if (opcao.equals("s")) {
 
-                try {
-                System.out.println("\nDescrição");
-                tarefa.setDescricao(read.nextLine());
-                list.add(tarefa);
+                boolean descricaoValida = false;
+                do {
 
-                } catch (IllegalArgumentException e){
-                    System.out.println(e.getMessage());
-                }
+                    try {
+
+                        System.out.println("\nDescrição");
+                        tarefa.setDescricao(read.nextLine());
+                        descricaoValida = true;
+
+                    } catch (IllegalArgumentException e) {
+
+                        System.out.println(e.getMessage());
+
+                    }
+
+                } while (!descricaoValida);
             }
 
-                
             else {
 
                 tarefa.setDescricao("Sem Descrição");
             }
 
+            list.add(tarefa);
+
             System.out.println("\nAdicionar mais outra tarefa? s/n");
             continuarTarefa = read.nextLine();
 
-            list.add(tarefa);
+        } while (continuarTarefa.equalsIgnoreCase("s"));
 
-        } while (continuarTarefa.equals("s"));
-
-        for (Tarefa t : list){
+        for (Tarefa t : list) {
 
             System.out.println("-----------------------------");
             System.out.println(t);
         }
 
-            
-        
     }
-    
+
 }
