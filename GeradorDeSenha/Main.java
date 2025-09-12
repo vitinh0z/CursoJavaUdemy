5aa89a97a05e7fa28ec6b6f38cedd120adc83411
vitinh0z <victor10.vg23@gmail.com>
2025-09-12 16:15:19 -0300

    fiz o menu mas tem algo errradoo

--- GeradorDeSenha/Main.java
+++ GeradorDeSenha/Main.java
@@ -13,20 +13,72 @@ public class Main {
         System.out.print("[1] - Gerar Senha\n[2] - Abrir Cofre\n[3] - Fechar Cofre\nOpção: ");
         int menu = read.nextInt();
 
+        read.nextLine();
+
         GerarSenha gerarSenha = new GerarSenha();
 
         System.out.println(gerarSenha);
 
         switch (menu) {
+
             case 1:
 
                 System.out.println("---- Gerar Senha ----");
                 System.out.println("Deve conter Número?: (s/n)");
                 char opcao = read.nextLine().charAt(0);
 
-                if (opcao.equals(){
-                    
+                if (opcao == 's'){
+                    gerarSenha.isIncluir_numeros();
                 }
+                else {
+                    System.out.println("Deve conter Minusculas?: (s/n)");
+                    opcao = read.nextLine().charAt(0);
+
+                    if (opcao == 's'){
+                        gerarSenha.isIncluir_numeros();
+                    }
+
+                    else {
+
+                        System.out.println("Deve conter maiusculas?: (s/n)");
+                        opcao = read.nextLine().charAt(0);
+
+                        if (opcao == 's'){
+                            gerarSenha.isIncluir_maiusculas();
+                        }
+
+                        else {
+                            System.out.println("Deve conter números?: (s/n)");
+                            opcao = read.nextLine().charAt(0);
+
+                            if (opcao == 's'){
+                                gerarSenha.isIncluir_numeros();
+                            }
+
+                            else {
+                                System.out.println("Deve conter Caracteres Especiais?: (s/n)");
+                                opcao = read.nextLine().charAt(0);
+
+                                if (opcao == 's'){
+                                    gerarSenha.isIncluir_simbolos();
+                                }
+
+                            }
+
+
+                            System.out.println("Senha: " + gerarSenha.gerarSenha());
+
+
+
+                        }
+
+                    }
+                }
+
+
+                
+
+                
                     
             break;
                 
