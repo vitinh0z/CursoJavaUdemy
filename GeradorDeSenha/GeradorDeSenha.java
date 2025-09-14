515c5ff7180a06711f722d63356f82bb0a4b9ca0
vitinh0z <victor10.vg23@gmail.com>
2025-09-14 15:26:47 -0300

    arrumei os ifs

--- GeradorDeSenha/GeradorDeSenha.java
+++ GeradorDeSenha/GeradorDeSenha.java
@@ -11,57 +11,68 @@ public class GeradorDeSenha {
 
         System.out.println("---- Gerar Senha ----");
 
-                    System.out.print("Qual o tamanho da senha: ");
-                    int tamanho = read.nextInt();
-                    read.nextLine();
+        System.out.print("Qual o tamanho da senha: ");
+        int tamanho = read.nextInt();
+        read.nextLine();
 
-                    gerarSenha.setTamanho(tamanho);
+        gerarSenha.setTamanho(tamanho);
 
-                    System.out.print("Deve conter letras minúsculas? (s/n): ");
-                    char opcao = read.nextLine().charAt(0);
-                    if (opcao == 's' || opcao == 'S') {
-                        gerarSenha.setIncluir_minusculas(true);
-                    }
+        System.out.print("Deve conter letras minúsculas? (s/n): ");
+        char opcao = read.nextLine().charAt(0);
 
-                    System.out.print("Deve conter letras maiúsculas? (s/n): ");
-                    opcao = read.nextLine().charAt(0);
-                    if (opcao == 's' || opcao == 'S') {
-                        gerarSenha.setIncluir_maiusculas(true);
-                    }
+        if (opcao == 's' || opcao == 'S') {
 
-                    System.out.print("Deve conter números? (s/n): ");
-                    opcao = read.nextLine().charAt(0);
-                    if (opcao == 's' || opcao == 'S') {
-                        gerarSenha.setIncluir_numeros(true);
-                    }
+            gerarSenha.setIncluir_minusculas(true);
+        }
 
-                    System.out.print("Deve conter símbolos? (s/n): ");
-                    opcao = read.nextLine().charAt(0);
-                    if (opcao == 's' || opcao == 'S') {
-                        gerarSenha.setIncluir_simbolos(true);
-                    }
+        System.out.print("Deve conter letras maiúsculas? (s/n): ");
+        opcao = read.nextLine().charAt(0);
 
-                    if (!gerarSenha.isIncluir_minusculas() && !gerarSenha.isIncluir_maiusculas()
-                            && !gerarSenha.isIncluir_numeros() && !gerarSenha.isIncluir_simbolos()) {
-                        // ----> se não for nenhum dos dois imprimi essa mensagem;
+        if (opcao == 's' || opcao == 'S') {
 
-                        System.out.println("Você deve escolher pelo menos um tipo de caractere para gerar a senha.");
+            gerarSenha.setIncluir_maiusculas(true);
+        }
 
-                    } else {
+        System.out.print("Deve conter números? (s/n): ");
 
-                        String senhaGerada = gerarSenha.gerarSenha();
+        opcao = read.nextLine().charAt(0);
+        if (opcao == 's' || opcao == 'S') {
 
-                        FileWriter arquivo = new FileWriter("senha.txt", true);
-                        PrintWriter gravarArq = new PrintWriter(arquivo);
+            gerarSenha.setIncluir_numeros(true);
+        }
 
-                        System.out.println("Onde será usada essa senha: ");
-                        String legenda = read.nextLine();
+        System.out.print("Deve conter símbolos? (s/n): ");
+        opcao = read.nextLine().charAt(0);
 
-                        gravarArq.printf(legenda + ": " + " " + senhaGerada + " \n");
+        if (opcao == 's' || opcao == 'S') {
 
-                        arquivo.close();
 
-                    }
+            gerarSenha.setIncluir_simbolos(true);
+        }
+
+        if (!gerarSenha.isIncluir_minusculas() && !gerarSenha.isIncluir_maiusculas() && !gerarSenha.isIncluir_numeros() && !gerarSenha.isIncluir_simbolos()) {
+            // ----> se não for nenhum dos dois imprimi essa mensagem;
+
+            System.out.println("Você deve escolher pelo menos um tipo de caractere para gerar a senha.");
+
+        } 
+                    
+                    
+        else {
+
+            String senhaGerada = gerarSenha.gerarSenha();
+
+            FileWriter arquivo = new FileWriter("senha.txt", true);
+            PrintWriter gravarArq = new PrintWriter(arquivo);
+
+            System.out.println("Onde será usada essa senha: ");
+            String legenda = read.nextLine();
+
+            gravarArq.printf(legenda + ": " + " " + senhaGerada + " \n");
+
+            arquivo.close();
+
+        }
 
         
     }
