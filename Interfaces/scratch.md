4565371324810795dc9eb2d2b025d8c23352e289
vitinh0z <victor10.vg23@gmail.com>
2025-09-28 15:23:30 -0300

    exericicio muito complicado. Ainda não foi usado interfaces

---
+++ Interfaces/scratch.md
@@ -0,0 +1,13 @@
+# Interface
+
+- Interface é um tipo que define um conjunto de operações que uma clsse deve implementar
+
+A interface estabelece um **contrato** que a classe deve cumprir.
+
+        interface Shape (){
+            double area();
+            double perimeter();
+        }
+
+- Pra que usar Interfaces
+  - para criar sistemas com baixo acomplamento e flexiveis 
---
+++ Interfaces/src/aplication/Main.java
@@ -0,0 +1,49 @@
+package aplication;
+
+import model.entities.CarRental;
+import model.entities.Invoice;
+import model.entities.Vehicle;
+import model.services.BrazilTaxService;
+import model.services.RentalService;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+
+        System.out.println("Entre com dados do aluguel: ");
+
+        System.out.println("Modelo do Carro: ");
+        String carModel = scanner.nextLine();
+
+        System.out.println("Retirada do Veiculo (dd/MM/yyyy hh:mm): ");
+        LocalDateTime start = LocalDateTime.parse(scanner.nextLine(), dateTimeFormatter);
+
+        System.out.println("Retorno do Veiculo (dd/MM/yyyy hh:mm): ");
+        LocalDateTime finish = LocalDateTime.parse(scanner.nextLine(), dateTimeFormatter);
+
+        CarRental carRental = new CarRental(start, finish, new Vehicle(carModel));
+
+        System.out.println("Entre com valor por hora: ");
+        double pricePerHour = scanner.nextDouble();
+
+        System.out.print("Entre com valor por dia: ");
+        double pricePerDay = scanner.nextDouble();;
+
+        RentalService rentalService = new RentalService( new BrazilTaxService(), pricePerDay, pricePerHour);
+
+        rentalService.processInvoce(carRental);
+
+        System.out.print("FATURA: ");
+        System.out.println("Pagamento Basico: " + carRental.getInvoice().getBasicPayment());
+        System.out.println("Imposto: " + carRental.getInvoice().getTax());
+        System.out.println("Pagamento total: " + carRental.getInvoice().getTotalPayment());
+
+    }
+}
