84d90b6178b83bf1a8d0ae6a3ef3e7bb1d5b7e74
vitinh0z <victor10.vg23@gmail.com>
2025-10-27 22:00:12 -0300

    estudando sobre predicate

--- Predicate/src/Main.java
+++ Predicate/src/Main.java
@@ -1,28 +1,27 @@
 import entities.Product;
-import entities.myComparator;
+import util.ProductPredicate;
 
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
+import java.util.function.Predicate;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
         List<Product> list = new ArrayList<>();
 
         list.add(new Product("TV", 900.0));
         list.add(new Product("Notebook", 1200.0));
-        list.add(new Product("Tablet", 450.0);
+        list.add(new Product("Tablet", 450.0));
+        list.add(new Product("HD Case", 80.98));
 
 
-        list.sort((o1, o2) -> o1.getName().toUpperCase().compareTo(o2.getName().toUpperCase()));
+        list.removeIf(p -> p.getPrice() >= 100);
 
 
-        for (Product p : list){
-            System.out.println(p);
+        for (Product product : list) {
+            System.out.println(product);
         }
 
 
