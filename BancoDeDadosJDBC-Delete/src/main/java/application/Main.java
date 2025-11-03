51bf75c9d9a4ae57c0c153eef5cd15e48f5cfb1f
vitin0z <victor10.vg23@gmail.com>
2025-11-03 15:45:36 -0300

    aprendi como atualizar dados no banco

---
+++ BancoDeDadosJDBC-Delete/src/main/java/application/Main.java
@@ -0,0 +1,45 @@
+package application;
+
+import db.DB;
+
+import main.java.db.DbException;
+
+import java.sql.*;
+
+import java.text.SimpleDateFormat;
+
+public class Main {
+
+    public static void main (String[] args){
+
+
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+
+        try {
+            connection = DB.getConnection();
+
+            preparedStatement = connection.prepareStatement("UPDATE seller "
+                    + "SET BaseSalary = BaseSalary + ? "
+                    + "WHERE "
+                    + "(DepartmentId = ?)"
+
+            );
+
+            preparedStatement.setDouble(1, 200);
+            preparedStatement.setInt(2, 2);
+
+            int rowsAffected = preparedStatement.executeUpdate();
+
+            System.out.println("DONE: " + rowsAffected);
+        } catch (SQLException e ){
+            e.printStackTrace();
+        }
+
+        finally {
+            DB.closeStatament(preparedStatement);
+            DB.closeConnection();
+        }
+
+    }
+}
