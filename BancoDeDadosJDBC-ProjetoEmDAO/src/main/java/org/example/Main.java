a47ff0b1d69c230f87ded6fabf96cd05e0e619d9
vitin0z <victor10.vg23@gmail.com>
2025-11-04 15:42:43 -0300

    criando a classe seller

--- BancoDeDadosJDBC-ProjetoEmDAO/src/main/java/org/example/Main.java
+++ BancoDeDadosJDBC-ProjetoEmDAO/src/main/java/org/example/Main.java
@@ -1,6 +1,9 @@
 package org.example;
 
 import entities.Department;
+import entities.Seller;
+
+import java.util.Date;
 
 public class Main {
 
@@ -9,6 +12,10 @@ public class Main {
 
         Department obj = new Department(1, "Books");
         System.out.println(obj);
+
+
+        Seller seller = new Seller(21, "Bob Brown", " bob@gmail.com", new Date(), 300.0, obj);
+        System.out.println(seller);
 
     }
 }
