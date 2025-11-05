0e18bd38aec6a85e78edfec9057432fb8b4db006
vitin0z <victor10.vg23@gmail.com>
2025-11-05 14:32:59 -0300

    implementação do insert

--- BancoDeDadosJDBC-ProjetoEmDAO/src/main/java/model/dao/impl/SellerDaoJDBC.java
+++ BancoDeDadosJDBC-ProjetoEmDAO/src/main/java/model/dao/impl/SellerDaoJDBC.java
@@ -62,6 +62,30 @@ public class SellerDaoJDBC implements SellerDao {
     @Override
     public void update(Seller seller) {
 
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+
+        try {
+            preparedStatement = connection.prepareStatement(
+                    "UPDATE seller SET Name = ?, Email = ?, BirthDate = ?, BaseSalary = ?, DepartmentId = ? WHERE Id = ?"
+            );
+
+            preparedStatement.setString(1, seller.getName());
+            preparedStatement.setString(2, seller.getEmail());
+            preparedStatement.setDate(3, new java.sql.Date(seller.getBirthDate().getTime()));
+            preparedStatement.setDouble(4, seller.getBaseSalary());
+            preparedStatement.setInt(5, seller.getDepartment().getId());
+            preparedStatement.setInt(6, seller.getId());
+
+            preparedStatement.executeUpdate();
+
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+
+        } finally {
+            DB.closeStatament(preparedStatement);
+            DB.closeResultSet(resultSet);
+        }
     }
 
     @Override
